b1a91ed6ffe1f3974bfb9d55f19b704622edac41
Tkachuk Ilya <tim@ip-46-73-218-65.bb.netbynet.ru>
2015-05-07 01:27:20 +0300

    some fixes

--- lib/mix/tasks/myswt.reset.ex
+++ lib/mix/tasks/myswt.reset.ex
@@ -3,10 +3,10 @@ defmodule Mix.Tasks.Myswt.Reset do
 	def run(_) do
 		case File.exists?("./priv") do
 			true -> 
+				IO.puts "FAIL, priv dir is already exist!"
+			false ->
 				File.cp!(Exutils.priv_dir(:myswt), "./priv")
 				IO.puts "SUCCESS, priv dir with MYSWT template created!"
-			false ->
-				IO.puts "FAIL, priv dir is already exist!"
 		end
 	end
 end
