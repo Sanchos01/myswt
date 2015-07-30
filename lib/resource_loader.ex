c1dc058e226764f9318bd011cc3bdf0d54725d16
tim <llStrainll@gmail.com>
2015-07-30 23:23:11 +0300

    add basic auth to index.html

--- lib/resource_loader.ex
+++ lib/resource_loader.ex
@@ -1,13 +1,13 @@
 defmodule Myswt.ResourceLoader do
 	use Silverb, [{"@basic_auth", :application.get_env(:myswt, :basic_auth, nil)}]
-	def init(req, path) do
-		IO.inspect path
+	def init(_, req, [path]) do
 		case {:cowboy_req.parse_header("authorization", req), @basic_auth} do
-			{{:basic, login, password}, %{login: login, password: password}} -> File.read!(path) |> reply(req)
+			{{:ok, {"basic",{login,password}}, req}, %{login: login, password: password}} -> File.read!(path) |> reply(req)
 			{_, none} when (none == nil) -> File.read!(path) |> reply(req)
-			_ -> {:ok, :cowboy_req.reply(401, [{"WWW-Authenticate", "Basic realm=\"wwwest server\""},{"connection","close"}], "", req), nil}
+			_ -> {:shutdown, :cowboy_req.reply(401, [{"WWW-Authenticate", "Basic realm=\"myswt server\""},{"connection","close"}], "", req) |> elem(1), nil}
 		end
 	end
-	def terminate(_reason, _req, _state), do: :ok
-	defp reply(ans, req), do: {:ok, :cowboy_req.reply(200, [{"Content-Type","text/html; charset=utf-8"},{"connection","close"}], ans, req), nil}
+	def handle(req, _), do: {:ok, req, nil}
+	def terminate(_,_,_), do: :ok
+	defp reply(ans, req), do: {:ok, :cowboy_req.reply(200, [{"Content-Type","text/html; charset=utf-8"},{"connection","close"}], ans, req) |> elem(1), nil}
 end
